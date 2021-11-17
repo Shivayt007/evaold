e350912964fd2eac81f9e9d9c64095e425fbc434
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-11-17 22:30:22 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -65,7 +65,7 @@ async def save_group(bot, message):
                         ]
                     )
                 )
-                    await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+                    await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title,), reply_markup=reply_markup)
         await asyncio.sleep(2)
 
 
