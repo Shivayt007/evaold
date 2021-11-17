9fbf6cbe7133e600aafd62933968392166b39b51
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-11-17 22:04:38 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -52,7 +52,7 @@ async def save_group(bot, message):
                     await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            temp.MELCOW['welcome'] = await script.send_video(
+            temp.MELCOW['welcome'] = await message.send_video(
                                          video= "https://telegra.ph/file/ec5404d035924f1113d8d.mp4",
                                          text= "Hello:- സുഖമാണ?",
        
